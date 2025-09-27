fade6d09725eb6b3f1862f4fd6940c7c7c43a0c9
Destiny <Dharris481@ivytech.edu>
2025-09-26 20:01:35 -0500

    Added TwoWayLinkedList implementation and supporting test/abstract/list files

--- BinaryFormatException.java
+++
@@ -1,9 +0,0 @@
-// Destiny Harris
-// Date: 09-10-2025
-// Assignment: Exercise 12.9
-
-public class BinaryFormatException extends Exception {
-    public BinaryFormatException(String message) {
-        super(message);
-    }
-}
